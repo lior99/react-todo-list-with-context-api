4a136648fb4f778a832a7d93ad0475ce74a6728a
Lior Shtivelman <lior.shtivelman@gmail.com>
2022-06-03 16:12:06 +0300

    Adding some styles

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -4,13 +4,25 @@ const ListItem = props => {
     const { item, onItemClick } = props;
     const { text, isComplete } = item;
 
-    const className = isComplete ? 'complete' : '';
+    const className = [
+        'description',
+        isComplete ? 'complete' : '',
+    ].join(' ')
+
+    const attrs = { checked: isComplete};
+
+    const onItemChange = () => {
+        console.log('you clicked on the checkbox');
+    }
 
     return (
         <li className="list-item" onClick={() => onItemClick(item)}>
-            <span className={className}>{ text }</span>
+            <input type="checkbox" name="checkboxItem" onChange={() => null} { ...attrs } /> 
+            <label htmlFor="checkboxItem" className={className}>{ text }</label>
         </li>
     );
 }
 
 export default ListItem;
+
+// onChange={() => onItemClick(item)}/>
